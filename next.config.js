ece2b1d186216c9f56be2fa78378acefc0f3c392
Your Name <your@email.com>
2025-02-23 03:46:35 +0400

    Add www to root domain redirect configuration in Next.js config

--- next.config.js
+++ next.config.js
@@ -14,6 +14,21 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'www.jacoosijaye.com',
+          },
+        ],
+        destination: 'https://jacoosijaye.com',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
